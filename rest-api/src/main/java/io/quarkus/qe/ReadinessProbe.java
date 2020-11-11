70617a3ae7651c8a4b72294db0fa2e60221649b4
Jose Carvajal <josecarvajalhilario@gmail.com>
2020-11-11 14:36:39 +0100

    Add formatter and rename artifacts (#3)

--- rest-api/src/main/java/io/quarkus/qe/ReadinessProbe.java
+++ rest-api/src/main/java/io/quarkus/qe/ReadinessProbe.java
@@ -5,7 +5,7 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
 
 @Readiness
-public class ReadinessProbe  implements HealthCheck {
+public class ReadinessProbe implements HealthCheck {
 
     @Override
     public HealthCheckResponse call() {
